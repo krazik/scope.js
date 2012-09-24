e5dcf22d69aa47cf6b5b7ea1a1e7d896fc7fbcb1
Rylan Hazelton <rylan@badger.com>
2012-09-24 14:44:55 -0700

    add blocking initialize support

--- core/initializers.js
+++ core/initializers.js
@@ -1,13 +1,24 @@
 with (scope()) {
   scope.__initializers = [];
-
-  define('initializer', function(callback) {
-    scope.__initializers.push(callback);
+  scope.__blocking_initializers = [];
+  
+  define('initializer', function(callback, blocking) {
+    (blocking ? scope.__blocking_initializers : scope.__initializers).push(callback);
+  });
+    
+  define('run_initializers', function() {
+    if (scope.__blocking_initializers.length) {
+      scope.__blocking_initializers.shift()();
+    }
+    else {
+      for (var i=0; i < scope.__initializers.length; i++) scope.__initializers[i]();
+      delete scope.__initializers;
+      delete scope.__blocking_initializers;
+    }
   });
   
   // setup browser hook
   window.onload = function() {
-    for (var i=0; i < scope.__initializers.length; i++) scope.__initializers[i]();
-    delete scope.__initializers;
+    run_initializers();
   }
 }
